892d6c032f25f37a51ecf90f20df08b0eb8fd6ab
Qinka <qinka@live.com>
2015-12-13 20:46:42 +0800

    update

--- Main.hs
+++ Main.hs
@@ -8,12 +8,12 @@ module Main
     main
     ) where
 
-      import Web.Scotty(scotty,get,file,setHeader)
+      import Web.Scotty(scotty,get,setHeader,text)
       import Network.Info(getNetworkInterfaces,NetworkInterface(..))
       import System.Directory(getAppUserDataDirectory,createDirectory,doesDirectoryExist)
       import Control.Monad(unless)
-      import Data.Time(getCurrentTime)
-      import Control.Concurrent(threadDelay,forkIO)
+      import Control.Monad.IO.Class(liftIO)
+      import Data.String(fromString)
 
 
       replace :: String -> String
@@ -30,25 +30,15 @@ module Main
         dataDir <- getAppUserDataDirectory "getip"
         doesDirectoryExist dataDir >>= (\x -> unless x $ createDirectory dataDir)
         putStrLn "GetIp server begin"
-        _ <- forkIO write
         scotty 7999 $
           get  "/" $ do
             setHeader "Content-Type" "application/json; charset=utf-8"
-            file $ dataDir ++ "/ipv4"
-
-      write :: IO ()
-      write = do
-        dataDir <- getAppUserDataDirectory "getip"
-        netinfo <- getNetworkInterfaces
-        let rt = getIp $ map (words.replace.show.ipv4) netinfo
-        case rt of
-          Nothing -> writeF (dataDir ++ "/ipv4") "CANNOT GET IPv4"
-          Just x -> writeF (dataDir ++ "/ipv4") x
-        threadDelay 60000000
-        write
-
-
-      writeF :: String -> String -> IO()
-      writeF fn text = do
-        time <- getCurrentTime
-        writeFile fn $ "{\"ipv4\":\"" ++ text ++ "\",\"refresh\":\"" ++ show time ++ "\"}"
+            netinfo <- liftIO getNetworkInterfaces
+            let rt = getIp $ map (words.replace.show.ipv4) netinfo
+            case rt of
+              Nothing -> do
+                liftIO $ putStrLn "CANNOT GET IPV$4"
+                text $"{\"status\":\"failed\",\"reason\":\"CANNOT GET IPV4\"}"
+              Just x -> do
+                liftIO $ putStrLn x
+                text $ fromString $ "{\"status\":\"success\",\"ipv4\":\""++x++"\"}"
